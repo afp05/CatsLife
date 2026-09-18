c8dde6c0d6bdd9cae29a8281e7e721dd00ca3179
afp05 <danchenkoandrew@gmail.com>
2026-09-18 10:51:12 +0200

    Complete basic task requirements

--- CatsLife/Program.cs
+++ CatsLife/Program.cs
@@ -1,10 +1,12 @@
 ﻿using CatsLifeServices.Models;
 using CatsLifeServices.Providers;
+using CatsLifeServices.Writers;
 
 
 HttpClient client = new HttpClient();
 ProviderAPI provider = new ProviderAPI(client);
 
 CatFact fact = await provider.GetFactAsync();
-
+FileWriterTXT filewritertxt = new FileWriterTXT();
 Console.WriteLine($"Length: {fact.Length}, Text: {fact.Fact}");
+await filewritertxt.WriteAsync(fact);
--- CatsLifeServices/Writers/FileWriterTXT.cs
+++ CatsLifeServices/Writers/FileWriterTXT.cs
@@ -1,11 +1,18 @@
-﻿using System;
+﻿using CatsLifeServices.Interfaces;
+using CatsLifeServices.Models;
+using System;
 using System.Collections.Generic;
 using System.Text;
 
 namespace CatsLifeServices.Writers
 {
-    internal class FileWriterTXT
+    public class FileWriterTXT : IFileWriter
     {
+        public async Task WriteAsync(CatFact fact)
+        {
+            string text = $"\"fact\": \"{fact.Fact}\", \"length\": {fact.Length}";
 
+            await File.AppendAllTextAsync("cats.txt",text+ Environment.NewLine);
+        }
     }
 }
