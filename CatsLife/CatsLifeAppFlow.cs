e81c181e230d1d95e0549d75dd6bf34f8908cd1e
Andrii Danchenko <danchenkoandrew@gmail.com>
2026-09-25 18:18:09 +0200

    Aded appsettings.json timesettings validation

--- CatsLife/CatsLifeAppFlow.cs
+++ CatsLife/CatsLifeAppFlow.cs
@@ -8,11 +8,11 @@ namespace CatsLife
 {
     public class CatsLifeAppFlow
     {
-      private readonly IGetFact _provider;
-      private readonly IFileWriter _fileWriter;
-      private readonly ILogger<CatsLifeAppFlow> _logger;
-      private readonly IConfiguration _configuration;   
-      
+        private readonly IGetFact _provider;
+        private readonly IFileWriter _fileWriter;
+        private readonly ILogger<CatsLifeAppFlow> _logger;
+        private readonly IConfiguration _configuration;
+
         public CatsLifeAppFlow(IGetFact provider, IFileWriter fileWriter, ILogger<CatsLifeAppFlow> logger, IConfiguration configuration)
         {
             _provider = provider;
@@ -22,19 +22,35 @@ namespace CatsLife
         }
 
         public async Task RunAsync()
-        {           
-            
-            int maxFacts = _configuration.GetValue<int>("TimeSettings:MaxFacts");
-            int intervalMiliSeconds = _configuration.GetValue<int>("TimeSettings:IntervalMiliSeconds");
+        {
 
-            
+            string? intervalValue = _configuration["TimeSettings:IntervalMiliSeconds"];
+            string? maxFactsValue = _configuration["TimeSettings:MaxFacts"];
+
+
+            if (!int.TryParse(maxFactsValue, out int maxFacts) || maxFacts <= 0)
+
+            {
+                Console.WriteLine("Invalid MaxFacts value. Program will use default value: 5");
+                maxFacts = 5;
+
+            }
+
+            if (!int.TryParse(intervalValue, out int intervalMiliSeconds) || intervalMiliSeconds < 0)
+
+            {
+                Console.WriteLine("Invalid IntervalMilliseconds value. Using default value: 1500 ms.");
+                intervalMiliSeconds = 1500;
+
+            }
+
             for (int i = 0; i < maxFacts; i++)
             {
-                
+
                 try
                 {
                     var fact = await _provider.GetFactAsync();
-                    await _fileWriter.WriteAsync(fact);            
+                    await _fileWriter.WriteAsync(fact);
                     Console.WriteLine(fact.Fact);
 
                     if (i < maxFacts - 1)
@@ -46,28 +62,28 @@ namespace CatsLife
                 {
                     Console.WriteLine($"An HTTP error occurred while fetching the cat fact. See log for details.");
                     _logger.LogError(ex, "An HTTP error occurred while fetching the cat fact. Details:");
-                  
+
                 }
                 catch (JsonException ex)
                 {
                     Console.WriteLine($"A JSON error occurred while processing the cat fact. See log for details.");
                     _logger.LogError(ex, "A JSON error occurred while processing the cat fact. Details:");
-                  
+
                 }
                 catch (IOException ex)
                 {
                     Console.WriteLine($"An I/O error occurred while writing the cat fact to file. See log for details.");
                     _logger.LogError(ex, "An I/O error occurred while writing the cat fact to file. Details:");
-                   
+
                 }
                 catch (UnauthorizedAccessException ex)
                 {
                     Console.WriteLine($"Access denied while writing the cat fact to file. See log for details.");
                     _logger.LogError(ex, "Access denied while writing the cat fact to file. Details:");
-                 
+
                 }
             }
-        
+
         }
     }
 }
